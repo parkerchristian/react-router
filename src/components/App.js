40a2a7b53670a6b07e54068e94dbf60dd7f3385e
Parker Helland <hellandp@gmail.com>
2019-05-20 11:31:58 -0700

    init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+      <Route />
+    </Router>
+  );
+}
